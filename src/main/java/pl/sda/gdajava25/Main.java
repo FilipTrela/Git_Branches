b7d74d10b4d80eea501ca3c3c89b936741d01078
Filip Trela <ftrela@gmail.com>
2019-06-11 18:11:47 +0200

    First plik_zapis commit

--- src/main/java/pl/sda/gdajava25/Main.java
+++ src/main/java/pl/sda/gdajava25/Main.java
@@ -1,7 +1,18 @@
 package pl.sda.gdajava25;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));
+            printWriter.println("Hello World!");
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
