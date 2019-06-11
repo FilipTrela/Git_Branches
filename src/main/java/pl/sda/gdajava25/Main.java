b5d86a2f5d825af084058efdf636e2cfdf023114
Filip Trela <ftrela@gmail.com>
2019-06-11 18:28:14 +0200

    Komit pierwszy

--- src/main/java/pl/sda/gdajava25/Main.java
+++ src/main/java/pl/sda/gdajava25/Main.java
@@ -1,7 +1,21 @@
 package pl.sda.gdajava25;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        int linie = 0;
+        try {
+            Scanner scanner = new Scanner(new FileReader("data.txt"));
+            while (scanner.hasNextLine()){
+                linie++;
+                String linia = scanner.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ilość lini "+linie);
     }
 }
